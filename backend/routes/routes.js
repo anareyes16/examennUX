5bd1135b5fdba9cb4a6a12bf59e578806b73b751
anareyes16 <anamereyes04@unitec.edu>
2025-09-22 14:08:54 -0600

    apis hechas
    completado el backend, hechas y probadas en psotman (también le di npm install por eso muchos archivos)
    listas: get/tours, get/tours/availability y put/tours/reserve

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -15,17 +15,52 @@ router.post('/login', async (req, res) => {
   res.json({ message: 'User login endpoint' });
 });
 
-router.get('/restaurants', async (req, res) => {
-  const limit = parseInt(req.query.limit) || 10;
-  const offset = parseInt(req.query.offset) || 0;
-  const sql = 'SELECT * FROM restaurants LIMIT $1 OFFSET $2';
+
+
+
+router.get('/tours/:limit/:offset', async (req, res) => {
+  try{
+    const { limit = 10, offset = 0 } = req.params;
+    const mostrar = await pool.query('SELECT * from tourapp.tours LIMIT $1 OFFSET $2', [limit, offset]);
+    res.json(mostrar.rows);
+   }catch(error){
+    console.error('Error al obtener los tours:', error);
+    res.status(500).json({ error: 'Error al obtener los tours' });
+   }
+  }
+);
+
+
+
+
+router.get('/tours/availability', async (req, res) => {
   try {
-    const result = await pool.query(sql, [limit, offset]);
-    res.json(result.rows);
+    const disponibilidad = await pool.query ('SELECT * FROM tourapp.tour_schedules WHERE schedule_time > NOW() AND seats_available > 0 ORDER BY schedule_time');    
+    res.json (disponibilidad.rows);
   } catch (err) {
-    console.error('Error fetching restaurants:', err);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error('No se puede encontrar lugar en esa fecha:', err);
+    res.status(500).json({ error: 'El servidor no funciona' });
+  } 
+});
+
+
+
+router.put('/tours/reserve', async (req, res) => {
+  const {personName,scheduleTime, tourId} = req.body;
+
+  if (!personName || !scheduleTime || !tourId) {
+    return res.status(400).json({ error: 'se le olvido escribir algo' });
+  }
+  try {
+    await pool.query('UPDATE tourapp.tour_schedules SET seats_available = seats_available - 1 WHERE schedule_time = $1 AND tour_id = $2 AND seats_available > 0', [scheduleTime, tourId]);
+    res.json({ message: 'Su reservacion se ha hecho!' });
+  } catch (error) {
+    console.error('Error al reservar el tour:', error);
+    res.status(500).json({ error: 'Error al reservar el tour' });
   }
 });
 
+
+
 module.exports = router;
+
